a33a120c8f81a2281e6993967f6cd577070586c7
xcv58 <chenyihonglove@gmail.com>
2016-08-05 18:19:31 -0700

    add CMD+Ctrl+j/k to control volume

--- hammerspoon/control.lua
+++ hammerspoon/control.lua
@@ -3,3 +3,22 @@ local hyper = {"cmd", "ctrl"}
 -- hs.hotkey.bind(hyper, "l", function()
 --     hs.caffeinate.systemSleep()
 -- end)
+
+function setVolume(n)
+    output = hs.audiodevice.defaultOutputDevice()
+
+    volume = output:outputVolume()
+    newVolume = volume + n
+
+    output:setVolume(newVolume)
+
+    hs.alert.show("Volume: " .. math.floor(output:outputVolume()))
+end
+
+hs.hotkey.bind(hyper, "k", function()
+    setVolume(1)
+end)
+
+hs.hotkey.bind(hyper, "j", function()
+    setVolume(-1)
+end)
