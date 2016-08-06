e5ced8d0b6a646aacd1cfbd55889665790232c82
xcv58 <chenyihonglove@gmail.com>
2016-08-06 11:34:57 -0700

    add mute/unmute feature

--- hammerspoon/control.lua
+++ hammerspoon/control.lua
@@ -7,6 +7,10 @@ local hyper = {"cmd", "ctrl"}
 function setVolume(n)
     output = hs.audiodevice.defaultOutputDevice()
 
+    if output:muted() then
+        setMuted(false)
+    end
+
     volume = output:outputVolume()
     newVolume = volume + n
 
@@ -15,10 +19,28 @@ function setVolume(n)
     hs.alert.show("Volume: " .. math.floor(output:outputVolume()))
 end
 
+function setMuted(mute)
+    output = hs.audiodevice.defaultOutputDevice()
+    output:setMuted(mute)
+    if mute then
+        hs.alert.show("Mute")
+    else
+        hs.alert.show("Not Mute")
+    end
+end
+
 hs.hotkey.bind(hyper, "k", function()
     setVolume(1)
 end)
 
 hs.hotkey.bind(hyper, "j", function()
     setVolume(-1)
+end)
+
+hs.hotkey.bind(hyper, "h", function()
+    setMuted(true)
+end)
+
+hs.hotkey.bind(hyper, "g", function()
+    setMuted(false)
 end)
