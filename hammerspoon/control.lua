55ecc02a7f31a5610b4663cf984588d7ac17ea64
Yihong Chen <yihongc@twitter.com>
2016-08-14 15:23:43 -0700

    volume control can remember last change

--- hammerspoon/control.lua
+++ hammerspoon/control.lua
@@ -1,8 +1,45 @@
 local hyper = {"cmd", "ctrl"}
 
+local INCREASE_VOLUME, DECREASE_VOLUME = 1, -1
+local lastTime = -1
+local lastDirection = 0
+local gap = 1
+
 -- hs.hotkey.bind(hyper, "l", function()
 --     hs.caffeinate.systemSleep()
 -- end)
+
+function isRecentChange (args)
+    local time = hs.timer.localTime()
+    local res = time - lastTime < 8
+    lastTime = time
+    return res
+end
+
+function isSameDirection (direction)
+    local res = lastDirection == direction
+    lastDirection = direction
+    return res
+end
+
+function increaseGap ()
+    gap = math.min(gap * 2, 16)
+end
+
+function resetGap ()
+    gap = 1
+end
+
+function changeVolume (direction)
+    local recentChange = isRecentChange()
+    local sameDirection = isSameDirection(direction)
+    if recentChange and sameDirection then
+        increaseGap()
+    else
+        resetGap()
+    end
+    setVolume(direction * gap)
+end
 
 function setVolume(n)
     output = hs.audiodevice.defaultOutputDevice()
@@ -30,11 +67,11 @@ function setMuted(mute)
 end
 
 hs.hotkey.bind(hyper, "k", function()
-    setVolume(1)
+    changeVolume(INCREASE_VOLUME)
 end)
 
 hs.hotkey.bind(hyper, "j", function()
-    setVolume(-1)
+    changeVolume(DECREASE_VOLUME)
 end)
 
 hs.hotkey.bind(hyper, "h", function()
