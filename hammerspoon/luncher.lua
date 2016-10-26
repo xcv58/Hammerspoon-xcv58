d9cf914d56512d124ce1c13fe300131bbc0654ae
xcv58 <i@xcv58.com>
2016-10-25 20:05:00 -0700

    rm VSCode from Editors

--- hammerspoon/luncher.lua
+++ hammerspoon/luncher.lua
@@ -1,7 +1,7 @@
 local hyper = {"cmd", "shift"}
 
 local browsers = {{"Google Chrome", "Safari"}}
-local editors = {{"com.microsoft.VSCode", "com.github.atom", "Sublime Text", "Emacs"}}
+local editors = {{"com.github.atom", "Sublime Text", "Emacs"}}
 local emails = {{"com.CloudMagic.MacMail"}}
 local chats = {{"HipChat", "WeChat", "Messages"}}
 local tweets = {{"Tweetbot"}}
