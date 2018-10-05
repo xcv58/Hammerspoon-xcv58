47bf58a6435e6d8e4bd422ed98cf05c0200c4d18
xcv58 <i@xcv58.com>
2018-10-05 16:56:26 -0700

    Temp commit

--- hammerspoon/control.lua
+++ hammerspoon/control.lua
@@ -91,3 +91,42 @@ end)
 hs.hotkey.bind(hyper, "l", function()
     hs.caffeinate.startScreensaver()
 end)
+
+
+local timer = nil
+function showTime (args)
+    hs.alert.show("test", hs.alert.defaultStype, hs.screen.mainScreen(), 1)
+end
+
+mouseCircle = nil
+mouseCircleTimer = nil
+
+
+hs.hotkey.bind(hyper, "y", function()
+    -- hs.dialog.alert("rest")
+    -- if timer then
+    --     timer:stop()
+    --     timer = nil
+    -- else
+    --     timer = hs.timer.doEvery(2, showTime)
+    -- end
+    -- Delete an existing highlight if it exists
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    -- Get the current co-ordinates of the mouse pointer
+    mousepoint = hs.mouse.getAbsolutePosition()
+    -- Prepare a big red circle around the mouse pointer
+    -- mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle = hs.drawing.text(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80), "test")
+    -- mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    -- mouseCircle:setFill(false)
+    -- mouseCircle:setStrokeWidth(5)
+    mouseCircle:show()
+
+    -- Set a timer to delete the circle after 3 seconds
+    mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
+end)
