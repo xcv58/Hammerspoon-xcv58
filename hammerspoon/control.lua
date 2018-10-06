b29629dcd4efedb56c1fc8cf3a54b7ba1ddfb0cd
Yihong <chenyihonglove@gmail.com>
2018-10-05 20:15:09 -0700

    Add CMD+Ctrl+t to show time on background (#10)

--- hammerspoon/control.lua
+++ hammerspoon/control.lua
@@ -91,3 +91,72 @@ end)
 hs.hotkey.bind(hyper, "l", function()
     hs.caffeinate.startScreensaver()
 end)
+
+
+local timer = nil
+function showTime (args)
+    hs.alert.show("test", hs.alert.defaultStype, hs.screen.mainScreen(), 1)
+end
+
+canvas = nil
+fadeTime = 0.5
+timeTimer = nil
+
+local function updateTime(x)
+    x.text = os.date("%I:%M:%S")
+end
+
+local function resetTimer()
+    if timeTimer then
+        timeTimer:stop()
+        timeTimer = nil
+    end
+end
+
+hs.hotkey.bind(hyper, "t", function()
+    local builtInScreen = hs.screen'Color LCD'
+    local cres = builtInScreen:fullFrame()
+    if canvas then
+        canvas:delete(fadeTime)
+        canvas = nil
+        resetTimer()
+        return
+    end
+
+    local textSize = math.min(cres.w, cres.h) / 3.5
+    textSize = math.max(textSize, 36)
+    local width = 5 * textSize
+    local height = 1.3 * textSize
+
+    canvas = hs.canvas.new({
+        x = (cres.w - width) / 2,
+        y = (cres.h - height) / 2,
+        h = height,
+        w = width,
+    }):show(fadeTime)
+    canvas:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
+    canvas:level(hs.canvas.windowLevels.desktopIcon)
+
+    canvas[1] = {
+        id = "rectangle",
+        type = "rectangle",
+        fillColor = {hex="#000", alpha=0.61},
+        action = "fill",
+    }
+    canvas[2] = {
+        id = "text",
+        type = "text",
+        textSize = textSize,
+        textAlignment = "center",
+        textFont = "Courier",
+        textLineBreak = "charWrap",
+    }
+    local text = canvas[2]
+
+    updateTime(text)
+    if timeTimer then
+        timeTimer:start()
+    else
+        timeTimer = hs.timer.doEvery(1, function() updateTime(text) end)
+    end
+end)
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,3 +9,19 @@ hs.alert.defaultStyle.textSize = 64
 -----------------------------------------------
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
+
+-- ModalMgr Spoon must be loaded explicitly, because this repository heavily relies upon it.
+hs.loadSpoon("ModalMgr")
+
+-- Define default Spoons which will be loaded later
+if not hspoon_list then
+  hspoon_list = {
+   "Calendar",
+    "CircleClock",
+  }
+end
+
+-- Load those Spoons
+for _, v in pairs(hspoon_list) do
+  hs.loadSpoon(v)
+end
