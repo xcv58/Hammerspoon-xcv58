2243a6ddf1f5d1b9ec9785673d5440aa19cf4b9c
xcv58 <i@xcv58.com>
2022-02-08 09:11:17 -0800

    Add zoom.us to chats

--- hammerspoon/launcher.lua
+++ hammerspoon/launcher.lua
@@ -20,7 +20,7 @@ local emails = {{
     "Gmail",
     "Google Calendar"
 }}
-local chats = {{"Slack", "WeChat", "Telegram", "Messages", "FaceTime", "Quip"}}
+local chats = {{"Slack", "WeChat", "Telegram", "Messages", "FaceTime", "Quip", "zoom.us"}}
 local tweets = {{"Tweetbot"}}
 local terminals = {{"iTerm2", "iTerm.app"}}
 local reminders = {{"Reminders", "Quip"}}
