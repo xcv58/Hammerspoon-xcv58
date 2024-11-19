2ed2aee6b4564bc944fb4ed4afa81d3e0e2efeea
xcv58 <i@xcv58.com>
2024-11-19 10:56:57 -0800

    Replace deprecated hs.mouse.getAbsolutePosition

--- hammerspoon/Spoons/Windows.spoon/init.lua
+++ hammerspoon/Spoons/Windows.spoon/init.lua
@@ -22,7 +22,7 @@ local function get_window_under_mouse()
   -- Invoke `hs.application` because `hs.window.orderedWindows()` doesn't do it and breaks itself
   local _ = hs.application
 
-  local my_pos = hs.geometry.new(hs.mouse.getAbsolutePosition())
+  local my_pos = hs.geometry.new(hs.mouse.absolutePosition())
   local my_screen = hs.mouse.getCurrentScreen()
 
   return hs.fnutils.find(hs.window.orderedWindows(), function(w)
